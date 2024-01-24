cfc33ed017d19eb057d59a3e2a348ad2c485c46e
mingu0429 <mingu0429@gmail.com>
2024-01-24 21:25:37 +0900

    first commit

---
+++ BE/Meari/src/main/java/Team/Meari/Meari/MeariApplication.java
@@ -0,0 +1,11 @@
+package Team.Meari.Meari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MeariApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MeariApplication.class, args);
+	}
+
+}
