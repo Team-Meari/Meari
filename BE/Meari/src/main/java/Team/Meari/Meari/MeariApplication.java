2217d9621dea6179f3c14091eeb16dc8fb4b9e80
mingu0429 <mingu0429@gmail.com>
2024-01-26 09:59:35 +0900

    :sparkles: feat: second test

--- BE/Meari/src/main/java/Team/Meari/Meari/MeariApplication.java
+++ BE/Meari/src/main/java/Team/Meari/Meari/MeariApplication.java
@@ -3,7 +3,7 @@ package Team.Meari.Meari;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class MeariApplication {
+public class MeariApplication { //first commit
 	public static void main(String[] args) {
 		SpringApplication.run(MeariApplication.class, args);
 	}
