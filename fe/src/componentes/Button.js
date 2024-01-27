ee967a328e41bc9b58a08a0f98ae892ae3c5ccfc
Shbak <khbak111@naver.com>
2024-01-27 19:47:26 +0900

    feat: 기본세팅 다시

---
+++ fe/src/componentes/Button.js
@@ -0,0 +1,5 @@
+function Button({ usage, onClick }) {
+  return <button onClick={onClick}>{usage}</button>;
+}
+
+export default Button;
---
+++ fe/src/routes/Login.js
@@ -0,0 +1,10 @@
+// modal 방식으로 구현예정 -> 모달 구현 직접 한번 해보기
+function Login() {
+  return (
+    <div>
+      <h1>Here for Login</h1>
+    </div>
+  );
+}
+
+export default Login;
