b9013a5c7cbbabbef0ee7a462ba0f3379a9df7c0
mingu0429 <mingu0429@gmail.com>
2024-02-05 20:35:57 +0900

    :sparkles: feat: Member Entity 생성

---
+++ BE/Meari/src/main/java/Team/Meari/Meari/member/entity/Member.java
@@ -0,0 +1,33 @@
+package Team.Meari.Meari.member.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity @Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String nickname;
+    private MemberStatus memberStatus;
+
+
+    @NoArgsConstructor
+    public enum MemberStatus{
+        MEMBER_ACTIVE("활성 계정"),
+        MEMBER_QUIT("탈퇴한 계정");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
+}
