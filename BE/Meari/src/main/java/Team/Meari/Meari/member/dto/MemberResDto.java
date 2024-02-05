f7e09f5826c07fab5d1feff84ae0c98b5dc8d81e
mingu0429 <mingu0429@gmail.com>
2024-02-05 20:36:53 +0900

    :sparkles: feat: Member Dto 생성

---
+++ BE/Meari/src/main/java/Team/Meari/Meari/member/dto/MemberResDto.java
@@ -0,0 +1,22 @@
+package Team.Meari.Meari.member.dto;
+
+import Team.Meari.Meari.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class MemberResDto {
+    private Long memberId;
+    private String email;
+    private String nickname;
+    private String memberStatus;
+
+    public MemberResDto(Member member) {
+        this.memberId = member.getId();
+        this.email = member.getEmail();
+        this.nickname = member.getNickname();
+        this.memberStatus = member.getMemberStatus().getStatus();
+    }
+}
