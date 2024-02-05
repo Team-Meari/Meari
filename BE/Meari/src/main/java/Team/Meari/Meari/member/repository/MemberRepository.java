43e4b461276dc89b77391deee14c52712b498a36
mingu0429 <mingu0429@gmail.com>
2024-02-05 20:36:39 +0900

    :sparkles: feat: Member Repository 생성

---
+++ BE/Meari/src/main/java/Team/Meari/Meari/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package Team.Meari.Meari.member.repository;
+
+import Team.Meari.Meari.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickname(String nickname);
+}
