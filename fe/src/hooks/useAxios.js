c6f5932dd758b20f563a34c90e756131deb8b7fc
Shbak <khbak111@naver.com>
2024-02-20 18:13:39 +0900

    필요없는 코드 제거

--- fe/src/hooks/useAxios.js
+++ fe/src/hooks/useAxios.js
@@ -2,8 +2,6 @@ import defaultAxios from "axios";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useGetAxios = (config, type, axiosInstance = defaultAxios) => {
-  const queryClient = useQueryClient();
-
   const dataFetch = async () => {
     try {
       const response = await axiosInstance({
