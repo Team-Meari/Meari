4811d5f7042b53f34b534fa015e057cfc23dca8b
Seunghun <126738939+Shbak111@users.noreply.github.com>
2024-02-20 17:41:09 +0900

    [FE] 메아리 CRUD 및 폴링 추가, react-query 도입(#23)
    * ♻️ feat: useAxios 훅 react-query로 리팩토링(캐싱, state관리)
    
    * feat: 지도 포함 렌더링 최적화 작업중
    
    * ✨ feat: 폴링 + react-query를 통한 메아리 리스트 구현 및 지도에 표시 추가
    
    * feat: 폴링관련 조금 수정

--- fe/src/index.js
+++ fe/src/index.js
@@ -5,6 +5,10 @@ import MyPage from "./routes/MyPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignUp from "./routes/SignUp";
 import { GlobalStyle } from "./css/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -23,8 +27,9 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <GlobalStyle />
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
