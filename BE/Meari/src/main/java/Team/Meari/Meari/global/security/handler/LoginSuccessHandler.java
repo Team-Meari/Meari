0a7e2d691abb310fe4881e9295da8d19dc5fdc95
mingu0429 <mingu0429@gmail.com>
2024-02-26 17:25:48 +0900

    :sparkles: feat: Login Handler 구현

---
+++ BE/Meari/src/main/java/Team/Meari/Meari/global/security/handler/LoginSuccessHandler.java
@@ -0,0 +1,27 @@
+package Team.Meari.Meari.global.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        //List<GrantedAuthority> authorities = (List<GrantedAuthority>) authentication.getAuthorities();
+        log.info("# Authenticated successfully !");
+        log.info("# Id: {}", authentication.getName());
+        //log.info("# roles: {}", authorities.toString());
+    }
+}
