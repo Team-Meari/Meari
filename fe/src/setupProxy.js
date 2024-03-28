66a09b31a9ba0b70999b91d93ada2a90eab3f102
Shbak <khbak111@naver.com>
2024-03-28 22:03:06 +0900

    feat: proxy 설정

---
+++ fe/src/setupProxy.js
@@ -0,0 +1,11 @@
+// src/setupProxy.js
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/chats", {
+      target: "http://15.165.207.71:8080/", // 백엔드 서버의 주소
+      changeOrigin: true,
+    })
+  );
+};
