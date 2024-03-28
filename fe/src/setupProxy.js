63ab8f5a1bc0811402b9d9efe7e4e5d9df1f32ea
Shbak <khbak111@naver.com>
2024-03-28 22:14:27 +0900

    feat: 로그인 프록시

--- fe/src/setupProxy.js
+++ fe/src/setupProxy.js
@@ -8,4 +8,10 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   );
+  app.use(
+    createProxyMiddleware("/auth", {
+      target: "http://15.165.207.71:8080/", // 백엔드 서버의 주소
+      changeOrigin: true,
+    })
+  );
 };
