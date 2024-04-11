46668d300029fdfcbc356b73e77560475bfc26b4
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2024-04-11 13:46:50 +0900

    [BE] 로드밸런서 상태 검사 메서드 추가 (#39)
    * :sparkles: feat: 상태 테스트 메서드
    
    * :recycle: refactor: TokenDto 변경
    
    ---------
    
    Co-authored-by: mingu0429 <mingu0429@gmail.com>

--- BE/Meari/src/main/java/Team/Meari/Meari/global/security/config/WebConfig.java
+++ BE/Meari/src/main/java/Team/Meari/Meari/global/security/config/WebConfig.java
@@ -11,13 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080","http://localhost:63342", "http://www.localhost:3000", "http://localhost:3000")
+                .allowedOrigins("https://shoutmeari.site:8080", "http://shoutmeari.site:8080", "http://meari-s3-bucket.s3-website.ap-northeast-2.amazonaws.com/","https://fe--shoutmeari.netlify.app/","https://shoutmeari.netlify.app/api","https://shoutmeari.netlify.app/", "http://shoutmeari.netlify.app/", "http://localhost:8080","http://localhost:63342", "http://www.localhost:3000", "http://localhost:3000", "http://15.165.207.71:8080","https://fa44-180-68-125-6.ngrok-free.app")
+                .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+//                .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true)
                 .allowedHeaders("*")
-                .allowedOriginPatterns()
+                .allowedOriginPatterns("*")
                 .exposedHeaders("*");
     }
 }
