a7eacc0c6303992a6243b2b65176cb2d0d543295
mingu0429 <mingu0429@gmail.com>
2024-06-24 21:01:36 +0900

    :recycle: refactor: 간단한 리팩토링

--- BE/Meari/src/main/java/Team/Meari/Meari/global/security/config/WebConfig.java
+++ BE/Meari/src/main/java/Team/Meari/Meari/global/security/config/WebConfig.java
@@ -11,10 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("https://shoutmeari.site:8080", "http://shoutmeari.site:8080", "http://meari-s3-bucket.s3-website.ap-northeast-2.amazonaws.com/","https://fe--shoutmeari.netlify.app/","https://shoutmeari.netlify.app/api","https://shoutmeari.netlify.app/", "http://shoutmeari.netlify.app/", "http://localhost:8080","http://localhost:63342", "http://www.localhost:3000", "http://localhost:3000", "http://15.165.207.71:8080","https://fa44-180-68-125-6.ngrok-free.app")
+                .allowedOrigins("http://localhost:8080","http://localhost:63342", "http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-//                .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true)
                 .allowedHeaders("*")
